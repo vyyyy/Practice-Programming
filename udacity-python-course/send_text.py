1b6d9350bb113be006e64f9131e874e507554109
vyyyy <vpat5777@uni.sydney.edu.au>
2016-08-31 16:18:51 +1000

    rm keys and no

--- udacity-python-course/send_text.py
+++ udacity-python-course/send_text.py
@@ -3,13 +3,13 @@
 from twilio.rest import TwilioRestClient 
  
 # put your own credentials here 
-ACCOUNT_SID = "AC76f5be68b42f0bc0e6fe972b8cbe8f5e" 
-AUTH_TOKEN = "f4b4c247e95893b0d6061687422c69aa" 
+ACCOUNT_SID = X 
+AUTH_TOKEN = X 
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 message = client.sms.messages.create(body="learning twilio with udacity",
-                                     to="+61497861969",
-                                     from_="+61419865493")
+                                     to="+61400000000",
+                                     from_="+61400000000")
 
 print(message.sid)
