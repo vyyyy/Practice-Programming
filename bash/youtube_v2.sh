a7fb0dcaa0d6435c9f882ff14f02a43a10043791
vyyyy <vpat5777@uni.sydney.edu.au>
2017-02-12 19:32:35 +1100

    updated syntax

--- bash/youtube_v2.sh
+++ bash/youtube_v2.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# dependency: youtube-dl. (google it, install it.)
+# dependency: youtube-dl. (google it, install it, update if necessary.)
 # download videos from youtube and convert into audio files.
 
 # Usage:
 # At the commandline, change directory to desktop or to the location
 # where you want to download the audio files.
-# For example, 
+# For example,
 # me@mycomputer $ cd ~/desktop
 # me@mycomputer desktop $ chmod +x youtube_v2.sh
-# me@mycomputer desktop $ ./youtube_v2.sh paste_your_youtube_video_url_here add_another_url_here_separated_by_a_space add_as_many_urls_as_you_want  
+# me@mycomputer desktop $ ./youtube_v2.sh paste_your_youtube_video_url_here add_another_url_here_separated_by_a_space add_as_many_urls_as_you_want
 
 for arg in "$@";
 do
-    youtube-dl -F "$arg"
-    youtube-dl -f 140 "$arg"
-    echo "COMPLETE"
+    youtube-dl -F "$arg";
+    youtube-dl -f 140 "$arg";
+    echo "COMPLETE";
 done
 
 exit 0
-
